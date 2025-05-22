22ba9ce92f8c96860f45a60c0a7bd79ff6e48c8a
RexitEricSia <sia.delong@rexit.com>
2025-05-22 11:46:32 +0800

    testing

--- tutorial/src/main/java/com/rexit/tutorial/service/MessageService.java
+++ tutorial/src/main/java/com/rexit/tutorial/service/MessageService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class MessageService {
-
+    // FOR THE SAKE OF testing cicd actions
     public String sendMessage(String userName) {
         return "Hello, " + userName + ". Welcome to Spring Boot.";
     }
