6c8266232984685de8d532f69c49a6b363f74080
RexitEricSia <sia.delong@rexit.com>
2025-05-22 10:49:12 +0800

    TASK-270-adding comment

--- tutorial/src/main/java/com/rexit/tutorial/service/MessageService.java
+++ tutorial/src/main/java/com/rexit/tutorial/service/MessageService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MessageService {
 
+    // this is thailand project
     public String sendMessage(String userName) {
         return "Hello, " + userName + ". Welcome to Spring Boot.";
     }
